b5eaa7bb9b12c5b1a85db75cbd606c9a52559c94
anzhela <angela.tes3@gmail.com>
2026-08-15 23:16:53 +0300

    Add error handlers and author selection to book form

--- library/authentication/views.py
+++ library/authentication/views.py
@@ -112,3 +112,15 @@ def user_details_view(request, user_id):
     context = {'user_obj': user}
 
     return render(request, 'authentication/user_details.html', context=context)
+
+def bad_request(request, exception=None):
+    return render(request, '400.html', status=400)
+
+def permission_denied(request, exception=None):
+    return render(request, '403.html', status=403)
+
+def page_not_found(request, exception=None):
+    return render(request, '404.html', status=404)
+
+def server_error(request):
+    return render(request, '500.html', status=500)
