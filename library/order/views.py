3be616342bb38c97c7cbec9e4eefbca910a26488
anzhela <angela.tes3@gmail.com>
2026-08-15 22:54:13 +0300

    Add permissions, fix order/book

--- library/order/views.py
+++ library/order/views.py
@@ -1,34 +1,41 @@
 from django.shortcuts import render, redirect
-from django.http import Http404
 from django.utils import timezone
 from datetime import timedelta
 from order.models import Order
 from book.models import Book
 from authentication.models import CustomUser
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import Http404
+from django.contrib import messages
 
 LOAN_PERIOD = timedelta(weeks=2)
 
+@login_required
 def create_an_order(request):
     if request.method == 'POST':
         book_id = request.POST.get('book')
-        user_id = request.POST.get('user')
-
         book = Book.get_by_id(book_id)
-        user = CustomUser.get_by_id(user_id)
 
-        if book is None or user is None:
-            raise Http404("Book or user not found")
+        if book is None:
+            raise Http404("Book not found")
 
         plated_end_at = timezone.now() + LOAN_PERIOD
-        order = Order.create(user=user, book=book, plated_end_at=plated_end_at)
+        order = Order.create(user=request.user, book=book, plated_end_at=plated_end_at)
 
         if order is None:
-            return render(request, 'order/create_an_order.html', {'error': 'No copies available.'})
+            books = Book.objects.all()
+            return render(request, 'order/create_an_order.html', {'error': 'No copies available.', 'books': books})
 
-        return redirect('user_orders', user_id=user.id)
+        book.count -= 1
+        book.save()
 
-    return render(request, 'order/create_an_order.html')
+        return redirect('user_orders', user_id=request.user.id)
 
+    books = Book.objects.all()
+    return render(request, 'order/create_an_order.html', {'books': books})
+
+@login_required
+@permission_required('is_staff')
 def close_an_order(request, order_id):
     order = Order.get_by_id(order_id)
     if order is None:
@@ -38,10 +45,16 @@ def close_an_order(request, order_id):
         return redirect('list_of_orders')
     return render(request, 'order/close_an_order.html', {'order': order})
 
+@login_required
 def user_orders(request, user_id):
+    if not request.user.is_staff and request.user.id != user_id:
+        messages.error(request, "Not allowed")
+        return render(request, '403.html', status=403)
     orders = Order.objects.filter(user_id=user_id)
-    return render(request, 'order/user_orders.html', {'orders': orders})
- 
+    return render(request, 'order/user_orders.html', {'orders': orders, 'user_id': user_id})
+
+@login_required
+@permission_required('is_staff')
 def list_of_orders(request):
     orders = Order.objects.all()
     return render(request, 'order/list_of_orders.html', {'orders': orders})
